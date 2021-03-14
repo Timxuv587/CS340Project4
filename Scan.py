daa86386da3d6a6a02a240360ac9c36ccce7aedf
Timxuv587 <1430415000@qq.com>
2021-03-14 09:30:25 +0800

    test

--- Scan.py
+++ Scan.py
@@ -17,10 +17,10 @@ def scan(input, output):
         #get_ipv6_addresses(url)
         #get_http_server(url)
         #check_insecure_http(url)
-        #get_redirect_to(url)
+        get_redirect_to(url)
         #get_hst(url)
-        get_tls_version(url)
-        get_ca(url)
+        #get_tls_version(url)
+        #get_ca(url)
     output_f = open(output, "w")
     json.dump(dict, output_f, sort_keys=True, indent=4)
 
